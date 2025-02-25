6b7d71830ed44daafc62a63e212e247a320d240c
Razvan Bogdan <mr.razvan.bogdan@gmail.com>
2025-02-25 17:22:04 +0200

    Initial Commit

---
+++ Assets/Scripts/CatAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CatAnimation : MonoBehaviour
+{
+
+    public float speed = 1f; // Viteza plutirii
+    public float height = 0.5f; // Înălțimea maximă a oscilației
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newY = startPosition.y + Mathf.Sin(Time.time * speed) * height;
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
+    }
+
+}
